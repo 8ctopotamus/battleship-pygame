bed22533d7dfa7f1bce016e5c397313931d10ee8
Josh <zylo.codes@gmail.com>
2022-10-19 23:19:53 -0500

    rando grid cont'd

--- scratchpad.py
+++ scratchpad.py
@@ -2,8 +2,16 @@ from random import choice
 from pprint import pprint
 
 grid = list( map(lambda _ : [0]*10, range(10)) )
+for size in [5,4,3,3,2]:
+  processing = True
+  vert = choice([True, False])
+  # search for a space to put the ship
+  while processing:
+    # find an empty cell to start
+    startRowIdx = choice(range(10))
+    startColIdx = choice(range(10))
+    if grid[startRowIdx][startColIdx] == 0:
+      grid[startRowIdx][startColIdx] = 1
+      processing = False
 
 pprint(grid)
-
-for size in [5,4,3,3,2]:
-  print(choice(grid))
